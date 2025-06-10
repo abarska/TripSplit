73367468f2a4ddcd39a94287b7fbbb94cb31ec5e
Anastasiya <anastasiya.barska@gmail.com>
2025-06-10 13:42:39 +0300

    Fix Trip Details title in the Toolbar

--- app/src/main/java/com/anabars/tripsplit/ui/screens/AppScreens.kt
+++ app/src/main/java/com/anabars/tripsplit/ui/screens/AppScreens.kt
@@ -1,5 +1,6 @@
 package com.anabars.tripsplit.ui.screens
 
+import android.util.Log
 import androidx.annotation.StringRes
 import androidx.navigation.NamedNavArgument
 import com.anabars.tripsplit.R
@@ -11,7 +12,9 @@ sealed class AppScreens(
 ) {
     data object TripsScreen : AppScreens(ROUTE_TRIPS, R.string.title_trips)
     data object AddTripScreen : AppScreens(ROUTE_ADD_TRIP, R.string.title_new_trip)
-    data object JoinExistingTripScreen : AppScreens(ROUTE_EXISTING_TRIP, R.string.title_join_existing_trip)
+    data object JoinExistingTripScreen :
+        AppScreens(ROUTE_EXISTING_TRIP, R.string.title_join_existing_trip)
+
     data object SettingsScreen : AppScreens(ROUTE_SETTINGS, R.string.title_settings)
     data object TripDetailsScreen : AppScreens(ROUTE_TRIP_DETAILS, R.string.title_trip_details)
 
@@ -22,13 +25,17 @@ sealed class AppScreens(
         const val ROUTE_SETTINGS = "settings"
         const val ROUTE_TRIP_DETAILS = "trip_details"
 
-        fun fromRoute(route: String?): AppScreens? = when (route) {
-            TripsScreen.route -> TripsScreen
-            AddTripScreen.route -> AddTripScreen
-            JoinExistingTripScreen.route -> JoinExistingTripScreen
-            SettingsScreen.route -> SettingsScreen
-            TripDetailsScreen.route -> TripDetailsScreen
-            else -> null
+        private val allAppScreens = listOf(
+            TripsScreen,
+            AddTripScreen,
+            JoinExistingTripScreen,
+            SettingsScreen,
+            TripDetailsScreen
+        )
+
+        fun fromRoute(route: String?): AppScreens? {
+            if (route == null) return null
+            return allAppScreens.firstOrNull { it.route == route.substringBefore('/') }
         }
     }
 }
